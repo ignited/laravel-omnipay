d1124f8a89b82a91308ace0cb2fc4553eb47f00f
Valentin Ivaşcu <oriceon@gmail.com>
2015-03-18 09:02:42 +0200

    Update config.php to use short array

--- src/config/config.php
+++ src/config/config.php
@@ -1,19 +1,20 @@
 <?php
 
-return array(
+return [
 
 	// The default gateway to use
 	'default' => 'paypal',
 
 	// Add in each gateway here
-	'gateways' => array(
-		'paypal' => array(
-			'driver' => 'PayPal_Express',
-			'options' => array(
-				'solutionType' => '',
-				'landingPage' => '',
+	'gateways' => [
+		'paypal' => [
+			'driver'  => 'PayPal_Express',
+			'options' => [
+				'solutionType'   => '',
+				'landingPage'    => '',
 				'headerImageUrl' => ''
-			)
-		)
-	)
-);
+			]
+		]
+	]
+
+];
